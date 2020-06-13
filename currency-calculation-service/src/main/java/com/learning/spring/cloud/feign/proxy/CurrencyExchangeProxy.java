6312d2ae7fce200783b8e0afb740e626667ebd21
DemoUser <fnualkatomar@gmail.com>
2020-06-13 17:57:57 -0400

    Added the Zuul Api Gateway and updated the code accordingly

--- currency-calculation-service/src/main/java/com/learning/spring/cloud/feign/proxy/CurrencyExchangeProxy.java
+++ currency-calculation-service/src/main/java/com/learning/spring/cloud/feign/proxy/CurrencyExchangeProxy.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.learning.spring.cloud.model.CurrencyCalculatedModel;
 
 //@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//Blocking the below line so as to route the Feign client through Zuul Api Gateway rather calling directly
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//Blocking the above line so as to route the Feign client through Zuul Api Gateway rather calling directly. 
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyCalculatedModel retrieveExchangeValue(@PathVariable String from,@PathVariable String to);
 
 }
